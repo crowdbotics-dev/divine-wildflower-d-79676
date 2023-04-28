83a79e94a9237ec92288641bd064f45b9cb67dc0
Crowdbotics <engineering@crowdbotics.com>
2023-04-28 10:27:55 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/divine_wildflower_d_79676/MainActivity.java
@@ -0,0 +1,15 @@
+package com.divine_wildflower_d_79676;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "divine_wildflower_d_79676";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-wildflower-d-79676",
+  url: "https://divine-wildflower-d-79676.botics.co",
+  api: "https://divine-wildflower-d-79676.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
